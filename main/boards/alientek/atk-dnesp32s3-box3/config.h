fb378cfbd887f60f46df2713537eb2fcb2988ae2
lty88815 <133635553+lty88815@users.noreply.github.com>
2026-09-07 23:03:46 +0800

    Update camera pin configurations in config.h

--- main/boards/alientek/atk-dnesp32s3-box3/config.h
+++ main/boards/alientek/atk-dnesp32s3-box3/config.h
@@ -74,21 +74,21 @@ enum PowerSupply {
 #define CAM_PIN_PWDN    GPIO_NUM_NC
 #define CAM_PIN_RESET   GPIO_NUM_NC
 #define CAM_PIN_VSYNC   GPIO_NUM_6
-#define CAM_PIN_LREF    GPIO_NUM_46
-#define CAM_PIN_PCLK    GPIO_NUM_45
-#define CAM_PIN_XCLK    GPIO_NUM_NC
-#define CAM_PIN_SIOD    GPIO_NUM_NC
-#define CAM_PIN_SIOC    GPIO_NUM_NC
-#define CAM_PIN_D0      GPIO_NUM_7
-#define CAM_PIN_D1      GPIO_NUM_8
-#define CAM_PIN_D2      GPIO_NUM_9
-#define CAM_PIN_D3      GPIO_NUM_10
-#define CAM_PIN_D4      GPIO_NUM_11
-#define CAM_PIN_D5      GPIO_NUM_12
-#define CAM_PIN_D6      GPIO_NUM_4
-#define CAM_PIN_D7      GPIO_NUM_5
-#define CAM_2V8_EN      14
-#define CAM_RST         15
+#define CAM_PIN_LREF    GPIO_NUM_5
+#define CAM_PIN_PCLK    GPIO_NUM_7
+#define CAM_PIN_XCLK    GPIO_NUM_16
+#define CAM_PIN_SIOD    GPIO_NUM_4
+#define CAM_PIN_SIOC    GPIO_NUM_17
+#define CAM_PIN_D0      GPIO_NUM_8
+#define CAM_PIN_D1      GPIO_NUM_9
+#define CAM_PIN_D2      GPIO_NUM_10
+#define CAM_PIN_D3      GPIO_NUM_11
+#define CAM_PIN_D4      GPIO_NUM_12
+#define CAM_PIN_D5      GPIO_NUM_13
+#define CAM_PIN_D6      GPIO_NUM_14
+#define CAM_PIN_D7      GPIO_NUM_15
+#define CAM_2V8_EN     GPIO_NUM_NC
+#define CAM_RST        GPIO_NUM_NC
 
 
 #endif // _BOARD_CONFIG_H_
